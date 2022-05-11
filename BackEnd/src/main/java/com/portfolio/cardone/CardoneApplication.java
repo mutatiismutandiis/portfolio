1053f9513092958036449e166450deb42a705f6e
Pau Cardone <paulacardone16@gmail.com>
2022-05-11 18:03:43 -0300

    Creacion carpetas

---
+++ BackEnd/src/main/java/com/portfolio/cardone/CardoneApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.cardone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardoneApplication.class, args);
+	}
+
+}
