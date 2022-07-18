cd65593c31df5b2190244d9cd6e17fc0b6e7c400
Pau Cardone <96506186+mutatiismutandiis@users.noreply.github.com>
2022-07-18 18:50:23 -0300

    Parte12

--- BackEnd/src/main/java/com/portfolio/cardone/Entity/Persona.java
+++ BackEnd/src/main/java/com/portfolio/cardone/Entity/Persona.java
@@ -6,7 +6,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter @Setter
 @Entity
 public class Persona {
     @Id
