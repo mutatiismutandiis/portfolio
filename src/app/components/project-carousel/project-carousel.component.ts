8528c0eb5d90a69f1204d42428d36b14c48e5d5c
Paula Cardone <paula.cardone@buhlergroup.com>
2024-07-14 17:49:57 +0200

    portfolio section completed

--- src/app/components/project-carousel/project-carousel.component.ts
+++ src/app/components/project-carousel/project-carousel.component.ts
@@ -2,7 +2,11 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from '../card/card.component';
 import { ProjectService } from '../../services/project.service';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
+
+Swiper.use([Navigation, Pagination]);
 @Component({
   selector: 'app-project-carousel',
   standalone: true,
@@ -27,7 +31,24 @@ export class ProjectCarouselComponent implements OnInit {
     );
   }
 
+  ngAfterViewInit(): void {
+    this.initializeSwiper();
+  }
+
   getImageUrl(imagePath: string): string {
     return this.projectService.getImageUrl(imagePath);
+  }
+
+  initializeSwiper(): void {
+    new Swiper('.mySwiper', {
+      loop: this.projects.length > 1, // Enable loop only if there are more than 1 project
+      navigation: true,
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
   }
 }
