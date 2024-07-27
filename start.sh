f7dd7974517c3f651ebc0d7f72e891ce58f494e2
Paula Cardone <paula.cardone@buhlergroup.com>
2024-07-27 13:50:23 +0200

    udate

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Navigate to the backend directory and start the backend server
-cd backend
-node server.js &
-
-# Navigate back to the root and serve the Angular frontend
-cd ../dist/portfolio
-npx http-server -c-1 -p $PORT --proxy "http://localhost:$PORT?"
